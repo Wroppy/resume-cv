c003bbecf9fae4229bf0b792279c3eb112befe04
Weyman Wong <wxwong806@gmail.com>
2024-06-29 21:54:39 +1200

    added the roboto font to the webpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
+import { ThemeOptions, ThemeProvider, createTheme } from "@mui/material";
 import "./App.css";
 import CustomDivider from "./components/CustomDivider/CustomDivider";
 import CvHeading from "./components/CvHeading/CvHeading";
 import EducationSection from "./components/EducationSection/EducationSection";
 
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    primary: {
+      main: "#1e88e5",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+  },
+};
+
 function App() {
   return (
-    <div className="CV">
-      <CvHeading />
-      <CustomDivider />
-      <EducationSection />
-    </div>
+    <ThemeProvider theme={createTheme(themeOptions)}>
+      <div className="CV">
+        <CvHeading />
+        <CustomDivider />
+        <EducationSection />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/RecordComponent/RecordComponent.tsx
+++ src/components/RecordComponent/RecordComponent.tsx
@@ -6,7 +6,7 @@ type Props = { children: React.ReactNode; record: Record };
 const RecordComponent = (props: Props) => {
   return (
     <div className="Record">
-      <div className="RecordHeading">{props.record.name}</div>
+      <div className="RecordHeading" style={{fontWeight: "600"}}>{props.record.name}</div>
       <div className="RecordTimestamp">{props.record.timestamp}</div>
       <div className="RecordContent">{props.children}</div>
     </div>
