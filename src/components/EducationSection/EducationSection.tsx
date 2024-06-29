9ddb8574ba11d5692432f0ef482f679e43d9c918
Weyman Wong <wxwong806@gmail.com>
2024-06-29 22:03:34 +1200

    Finished education section of the cv

--- src/components/EducationSection/EducationSection.tsx
+++ src/components/EducationSection/EducationSection.tsx
@@ -9,18 +9,36 @@ const EducationSection = () => {
       name: "University of Auckland",
       timestamp: "2023 - Present",
     },
+    {
+      name: "Scots College",
+      timestamp: "2016 - 2022",
+    },
   ];
+
+  const educationContent: React.ReactNode[] = [
+    <>
+      <div>
+        Bachelor of Software Engineering (Honours) / Bachelor of Science
+        (Mathematics)
+      </div>
+      <div>GPA: 8.80/9.00</div>
+    </>,
+    <>
+      <div>NCEA Level 2 and 3 with Excellence Endorsement</div>
+      <div>
+        Relevant Subjects: Calculus, Physics, Chemistry, Digital Technologies,
+        English
+      </div>
+    </>,
+  ];
+
   return (
     <div className="EducationSection">
       <div className="EducationHeading">Education</div>
       <div className="EducationContent">
         {education.map((record, index) => (
           <RecordComponent key={index} record={record}>
-            <div>
-              Bachelor of Software Engineering (Honours) / Bachelor of Science
-              (Mathematics)
-            </div>
-            <div>GPA: 8.80/9.00</div>
+            {educationContent[index]}
           </RecordComponent>
         ))}
       </div>
