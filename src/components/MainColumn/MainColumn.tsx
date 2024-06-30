9a9697f4f207813740d8198c8e185d37f5177b78
Weyman Wong <wxwong806@gmail.com>
2024-06-30 20:51:13 +1200

    Added a personal statement to the cv

--- src/components/MainColumn/MainColumn.tsx
+++ src/components/MainColumn/MainColumn.tsx
@@ -4,10 +4,13 @@ import CustomDivider from "../CustomDivider/CustomDivider";
 import EducationSection from "../EducationSection/EducationSection";
 import AchievementsSection from "../AchievementsSection/AchievementsSection";
 import VolunteerAndCommunitySection from "../VolunteerAndCommunitySection/VolunteerAndCommunitySection";
+import PersonalStatementSection from "../PersonalStatementSection/PersonalStatementSection";
 
 const MainColumn = () => (
   <div className="MainColumn">
     <CvHeading />
+    <CustomDivider />
+    <PersonalStatementSection />
     <CustomDivider />
     <EducationSection />
     <CustomDivider />
---
+++ src/components/PersonalStatementSection/PersonalStatementSection.tsx
@@ -0,0 +1,15 @@
+import RecordSection from "../RecordSection/RecordSection";
+
+const PersonalStatementSection = () => {
+  return (
+    <RecordSection heading="Personal Statement">
+      I am an enthusiastic software engineering and mathematics student studying
+      at the University of Auckland with a keen interest in web development. I
+      am a highly motivated, resilient and patient individual, eager and excited
+      to use my academic knowledge to gain experience in real-world applications
+      and scenarios.
+    </RecordSection>
+  );
+};
+
+export default PersonalStatementSection;
