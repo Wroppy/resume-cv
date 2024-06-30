14eb1daa2ca63af0a5f1b19a851318c2300b2cad
Weyman Wong <wxwong806@gmail.com>
2024-06-30 12:07:01 +1200

    Changed cv font size to be smaller

--- src/components/RecordComponent/RecordComponent.tsx
+++ src/components/RecordComponent/RecordComponent.tsx
@@ -6,6 +6,7 @@ type Props = { children?: React.ReactNode; record: Record; bold?: boolean };
 const RecordComponent = ({ children, record, bold = true }: Props) => {
   const style = {
     fontWeight: bold ? "500" : "normal",
+    fontSize: bold ? "16px" : "14px",
   };
 
   return (
